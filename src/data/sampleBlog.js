6ccbfc9e1d0139ce27722527d6aa91f0c99137cb
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-31 11:49:34 +0300

    sample blog data

---
+++ src/data/sampleBlog.js
@@ -0,0 +1,20 @@
+const blogArr = [
+    {
+        title: 'New best show',
+        author: 'John Doe',
+    },
+    {
+        title: 'Titanic',
+        author: 'James Brown',
+    },
+    {
+        title: 'Shark in the ocean',
+        author: 'John Doe',
+    },
+    {
+        title: 'Blubries in the spring',
+        author: 'Kyle Kylen',
+    },
+]
+
+module.exports = blogArr
