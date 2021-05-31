889dee80991c6896a586f62b795dfc2ca2d615b9
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-05-31 11:09:29 +0300

    dynamic title for pages, nav link active class

--- src/index.js
+++ src/index.js
@@ -12,24 +12,34 @@ app.set('views', 'src/views');
 // home page
 app.get('/', function (req, res) {
   // paimti index.ejs faila is views direktorijos
-  res.render('index');
+  res.render('index', { 
+    title: 'Home',
+    page: 'home'
+  });
 });
 
 // about page
 app.get('/about', function (req, res) {
   res.render('about', {
     title: 'About us',
+    page: 'about'
   });
 });
 
 // blog page
 app.get('/blog', function (req, res) {
-  res.render('blog');
+  res.render('blog', {
+    title: "Our blog",
+    page: 'blog'
+  });
 });
 
 // contact page
 app.get('/contact', function (req, res) {
-  res.render('contact');
+  res.render('contact', {
+    title: 'Contact us',
+    page: 'contact'
+  });
 });
 
 const staticPath = path.join(__dirname, 'static');
