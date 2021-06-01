453f5e9712e1cd5672dd0c428bbf74b80fd50c2b
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-01 15:38:40 +0300

    formDataMagic

--- src/static/js/create.js
+++ src/static/js/create.js
@@ -2,23 +2,39 @@ console.log('create');
 
 import MyFetch from './class/MyFetch.class.js';
 
-MyFetch.getPosts()
-  .then((posts) => console.log('posts posts', posts))
-  .catch((err) => console.error(err.message));
+// MyFetch.getPosts()
+//   .then((posts) => console.log('posts posts', posts))
+//   .catch((err) => console.error(err.message));
 
 // get all form data into json object
 //and send it to back end via fetch
 
-const newPostData = {
-  title: 'New post about Birds',
-  author: 'James Cameron',
-  body: 'This is essential to know.....',
-};
+// const newPostData = {
+//   title: 'New post about Birds',
+//   author: 'James Cameron',
+//   body: 'This is essential to know.....',
+// };
 
-const jsonData = JSON.stringify(newPostData);
-MyFetch.createPost(jsonData, (data) => {
-  if (data.redirect) {
-    console.log('redirecting to ', data.redirect);
-    // window.location = data.redirect;
-  }
+// const jsonData = JSON.stringify(newPostData);
+// MyFetch.createPost(jsonData, (data) => {
+//   if (data.redirect) {
+//     console.log('redirecting to ', data.redirect);
+//     // window.location = data.redirect;
+//   }
+// });
+
+const mainForm = document.getElementById('create-post-form');
+
+mainForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  console.log('Stop right there, dont you dare to send it, I swear to God');
+
+  // supakuojam formos duomenis
+  const fd = new FormData(mainForm);
+  // form data to json conversija
+  const fdJsonFormat = JSON.stringify(Object.fromEntries(fd));
+
+  MyFetch.createPost(fdJsonFormat, (ats) => {
+    console.log(ats);
+  });
 });
