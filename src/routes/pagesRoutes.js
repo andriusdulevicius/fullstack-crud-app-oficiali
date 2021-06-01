c893d5644d02c2775f8e0d8b2710a8b25c56c7bd
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-01 09:30:51 +0300

    single page template works

--- src/routes/pagesRoutes.js
+++ src/routes/pagesRoutes.js
@@ -48,9 +48,14 @@ router.get('/blog/create', function (req, res) {
 
 // Single page route
 router.get('/single/:id', (req, res) => {
+  const blogId = req.params.id;
+  const found = blogDb.find((p) => p.id === +blogId);
+  // TODO: redirect if not found or no id given
+  console.log(' found', found);
   res.render('singlePage', {
     title: 'Post about ...',
     page: 'single',
+    post: found,
   });
 });
 // /single/:id
