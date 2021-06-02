82e136c7c0d94c503dfecf3016426f98ae9d9c6d
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-02 09:01:16 +0300

    redirect after success post creation

--- src/static/js/create.js
+++ src/static/js/create.js
@@ -2,27 +2,6 @@ console.log('create');
 
 import MyFetch from './class/MyFetch.class.js';
 
-// MyFetch.getPosts()
-//   .then((posts) => console.log('posts posts', posts))
-//   .catch((err) => console.error(err.message));
-
-// get all form data into json object
-//and send it to back end via fetch
-
-// const newPostData = {
-//   title: 'New post about Birds',
-//   author: 'James Cameron',
-//   body: 'This is essential to know.....',
-// };
-
-// const jsonData = JSON.stringify(newPostData);
-// MyFetch.createPost(jsonData, (data) => {
-//   if (data.redirect) {
-//     console.log('redirecting to ', data.redirect);
-//     // window.location = data.redirect;
-//   }
-// });
-
 const mainForm = document.getElementById('create-post-form');
 
 mainForm.addEventListener('submit', function (event) {
@@ -36,5 +15,9 @@ mainForm.addEventListener('submit', function (event) {
 
   MyFetch.createPost(fdJsonFormat, (ats) => {
     console.log(ats);
+    if (ats.redirect) {
+      // console.log('redirecting to', ats.redirect);
+      window.location = ats.redirect;
+    }
   });
 });
