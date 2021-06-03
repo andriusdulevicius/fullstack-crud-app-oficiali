1053d1600591ecc4d1ca6343f72cb973878120df
Marius Kuzminskas <kuzminskas.marius7@gmail.com>
2021-06-03 11:05:42 +0300

    create Bob op owner

--- src/routes/ownersRoutes.js
+++ src/routes/ownersRoutes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Owner = require('../models/owner');
+
 router.get('/', (req, res) => {
   res.render('owners/index', {
     title: 'Owners',
@@ -8,10 +10,23 @@ router.get('/', (req, res) => {
   });
 });
 router.get('/new', (req, res) => {
-  res.render('owners/new', {
-    title: 'Add owner',
-    page: 'owners_new',
+  // sukurti 3 naujus ownerius
+  const o1 = { name: 'Bob', email: 'Bob@Pop.op' };
+
+  const newOwner = new Owner({
+    name: o1.name,
+    email: o1.email,
   });
+  newOwner
+    .save()
+    .then((result) => {
+      res.render('owners/new', {
+        title: 'Add owner',
+        page: 'owners_new',
+        result,
+      });
+    })
+    .catch((err) => res.send('Opps did not save', err));
 });
 
 module.exports = router;
